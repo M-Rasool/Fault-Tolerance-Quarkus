40d5f69830aa9175d23138de1bee9ab1eeae540a
MRasool <rasoolavazeh@yahoo.com>
2020-12-02 12:28:03 +0330

    Using Retry 1

--- src/main/java/tech/donau/BookRepository.java
+++ src/main/java/tech/donau/BookRepository.java
@@ -1,16 +1,35 @@
 package tech.donau;
 
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.jboss.logging.Logger;
+import tech.donau.data.data.Book;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 @Path("/book")
 public class BookRepository {
 
+    private static Logger LOGGER = Logger.getLogger(BookRepository.class.getName());
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @Produces(MediaType.APPLICATION_JSON)
+    @Retry(maxRetries = 4, delay = 1000L)
+    public List<Book> getBooks() {
+
+        final boolean fail = new Random().nextBoolean();
+        if (fail) {
+            LOGGER.info("This exception is for test");
+            throw new RuntimeException("Test");
+        }
+        return Arrays.asList(new Book("1", "Book 1", "Author 1"),
+                            new Book("2", "Book 2", "Author 2"),
+                            new Book("3", "Book 3", "Author 3")
+        );
     }
 }
